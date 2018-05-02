2e4177e8689f69de20fb5c10b30c8eaf66220c29
Todd Hancock <thancock20@hotmail.com>
2018-05-01 22:25:05 -0500

    Add code component

---
+++ components/Code.js
@@ -0,0 +1,23 @@
+const Code = ({ children }) => (
+  <pre>
+    <code>
+      { children }
+    </code>
+    <style jsx>{`
+      code {
+        display: block;
+        color: #C7254E;
+        font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+        font-size: 13px;
+        line-height: 1.42857143;
+        background-color: #F9F2F4;
+        padding: 9.5px;
+        margin: 0 0 10px;
+        border: 1px solid #CCC;
+        border-radius: 4px;
+      }
+    `}</style>
+  </pre>
+)
+
+export default Code
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
-const Index = () => (<p>Hello, World!</p>)
+import Code from '../components/Code'
+
+const code = `Testing the code component
+Across mutiple lines`
+
+const Index = () => (
+    <React.Fragment>
+      <p>Hello, World!</p>
+      <Code>{ code }</Code>
+    </React.Fragment>
+)
 
 export default Index
