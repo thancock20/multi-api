a836c7b2a79350e14a7041f6544ff53c63b3bcc0
Todd Hancock <thancock20@hotmail.com>
2018-05-05 02:04:34 -0500

    Add custom server for api

---
+++ server.js
@@ -0,0 +1,36 @@
+const Koa = require('koa');
+const next = require('next');
+const Router = require('koa-router');
+const Api = require('./api/router');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+.then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  router.use('/api', Api.routes(), Api.allowedMethods());
+
+  router.get('*', async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  server.use(router.routes());
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+})
+.catch((ex) => {
+  console.error(ex.stack);
+  process.exit(1);
+})
