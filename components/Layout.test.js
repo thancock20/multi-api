ed1ab6e8a2e7221433415b6e8ddcfb55f530bf1d
Todd Hancock <thancock20@hotmail.com>
2018-05-07 18:14:38 -0500

    Add test to get full coverage of components

--- components/Layout.test.js
+++ components/Layout.test.js
@@ -1,10 +1,15 @@
 import Layout from './Layout';
 
 describe('Layout component', () => {
-  it('renders with title and children', () => {
+  it('renders with children and title', () => {
     const wrapper = shallow(
       <Layout title="Here's the title">Here's the children.</Layout>
     );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders with children and no title', () => {
+    const wrapper = shallow(<Layout>Here's the children.</Layout>);
     expect(wrapper).toMatchSnapshot();
   });
 });
