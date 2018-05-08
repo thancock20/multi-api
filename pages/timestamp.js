1c435cdea310f0525f64b2c843c7aaa6941c22aa
Todd Hancock <thancock20@hotmail.com>
2018-05-08 16:13:41 -0500

    Rework timestamp page and getHostname function to print correct protocol always

--- pages/timestamp.js
+++ pages/timestamp.js
@@ -2,34 +2,49 @@ import Code from '../components/Code';
 import Layout from '../components/Layout';
 import getHostname from '../utils/get-hostname';
 
-const Timestamp = ({ hostname }) => (
-  <Layout title="Timestamp">
-    <p>Implements the following User stories:</p>
-    <ul>
-      <li>
-        I can pass a string as a parameter, and it will check to see whether
-        that string contains either a unix timestamp or a natural language date
-        (example: January 1, 2016).
-      </li>
-      <li>
-        If it does, it returns both the Unix timestamp and the natural language
-        form of that date.
-      </li>
-      <li>
-        If it does not contain a date or Unix timestamp, it returns null for
-        those properties.
-      </li>
-    </ul>
+// eslint-disable-next-line no-undef
+class Timestamp extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <h2>Example usage:</h2>
-    <Code>{hostname}/api/timestamp/May 7, 2018</Code>
-    <Code>{hostname}/api/timestamp/1525669200</Code>
+    this.state = {
+      hostname: ''
+    };
+  }
 
-    <h2>Example Output:</h2>
-    <Code>{`{ "unix": "2525669200", "natural", "May 7, 2018" }`}</Code>
-  </Layout>
-);
+  componentDidMount() {
+    this.setState({ hostname: getHostname() });
+  }
 
-Timestamp.getInitialProps = ({ req }) => ({ hostname: getHostname(req) });
+  render() {
+    return (
+      <Layout title="Timestamp">
+        <p>Implements the following User stories:</p>
+        <ul>
+          <li>
+            I can pass a string as a parameter, and it will check to see whether
+            that string contains either a unix timestamp or a natural language
+            date (example: January 1, 2016).
+          </li>
+          <li>
+            If it does, it returns both the Unix timestamp and the natural
+            language form of that date.
+          </li>
+          <li>
+            If it does not contain a date or Unix timestamp, it returns null for
+            those properties.
+          </li>
+        </ul>
+
+        <h2>Example usage:</h2>
+        <Code>{this.state.hostname}/api/timestamp/May 7, 2018</Code>
+        <Code>{this.state.hostname}/api/timestamp/1525669200</Code>
+
+        <h2>Example Output:</h2>
+        <Code>{`{ "unix": "2525669200", "natural", "May 7, 2018" }`}</Code>
+      </Layout>
+    );
+  }
+}
 
 export default Timestamp;
--- utils/get-hostname.js
+++ utils/get-hostname.js
@@ -1,9 +1,5 @@
 /* eslint no-restricted-globals: 0 */
-export default req => {
-  if (req) {
-    return `http://${req.headers.host}`;
-  }
-  return location.hostname === 'localhost'
+export default () =>
+  location.hostname === 'localhost'
     ? `http://localhost:${location.port}`
     : `${location.protocol}//${location.hostname}`;
-};
